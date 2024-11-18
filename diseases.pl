51084685c571b878f91d866499b614205d945137
Joanna Kaczmarek <joannakaczmarek93@gmail.com>
2024-11-18 18:28:12 +0100

    health problems defined

--- diseases.pl
+++ diseases.pl
@@ -1,3 +1,28 @@
 Disease, Symptom, Patient = symbol
+patient_symptom(Patient, Symptom)
 hypothesis(Patient, Disease)
-hypothesis2(Patient, Disease)
+
+% possible health problems
+hypothesis(Patient, heart_attack) := 
+	patient_symptom(Patient, fatigue),
+	patient_symptom(Patient, breathing_problems),
+	patient_symptom(Patient, indigestion),
+	patient_symptom(Patient, abdominal_pain).
+	
+hypothesis(Patient, stroke) := 
+	patient_symptom(Patient, sight_problems),
+	patient_symptom(Patient, dizziness),
+	patient_symptom(Patient, confusion),
+	patient_symptom(Patient, speech_problems).
+	
+hypothesis(Patient, lung_disease) := 
+	patient_symptom(Patient, cough),
+	patient_symptom(Patient, breathing_problems),
+	patient_symptom(Patient, phlegm).
+	
+hypothesis(Patient, flu) := 
+	patient_symptom(Patient, cough),
+	patient_symptom(Patient, headache),
+	patient_symptom(Patient, fatigue),
+	patient_symptom(Patient, chills),
+	patient_symptom(Patient, fever).
