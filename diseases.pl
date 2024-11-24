a22af4386d6933feb51eaeda8536fed6c4abbb36
Joanna Kaczmarek <joannakaczmarek93@gmail.com>
2024-11-24 18:24:38 +0100

    file divided

--- diseases.pl
+++ diseases.pl
@@ -1,18 +1,3 @@
-patient_symptom(anna,fatigue).
-patient_symptom(anna,indigestion).
-patient_symptom(anna,abdominal_pain).
-patient_symptom(anna,breathing_problems).
-
-patient_symptom(jim,cough).
-patient_symptom(jim,fever).
-patient_symptom(jim,chills).
-
-patient_symptom(kate,chills).
-patient_symptom(kate,fatigue).
-patient_symptom(kate,cough).
-patient_symptom(kate,fever).
-patient_symptom(kate,headache).
-
 hypothesis(Patient,heart_attack) :- 
 	patient_symptom(Patient,fatigue),
 	patient_symptom(Patient,breathing_problems),
