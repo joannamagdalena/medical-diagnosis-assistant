0d37f81cc86cbbbf41ff9d49bd4923ef778d3290
Joanna Kaczmarek <joannakaczmarek93@gmail.com>
2024-11-24 18:25:04 +0100

    file divided

---
+++ assistant.pl
@@ -0,0 +1,16 @@
+patient_symptom(anna,fatigue).
+patient_symptom(anna,indigestion).
+patient_symptom(anna,abdominal_pain).
+patient_symptom(anna,breathing_problems).
+
+patient_symptom(jim,cough).
+patient_symptom(jim,fever).
+patient_symptom(jim,chills).
+
+patient_symptom(kate,chills).
+patient_symptom(kate,fatigue).
+patient_symptom(kate,cough).
+patient_symptom(kate,fever).
+patient_symptom(kate,headache).
+
+:- include('diseases.pl').
