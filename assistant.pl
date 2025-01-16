67968b453a6e1bb305e27965878ad89e23d4983d
Joanna Kaczmarek <joannakaczmarek93@gmail.com>
2025-01-16 21:52:03 +0100

    questions added

--- assistant.pl
+++ assistant.pl
@@ -14,3 +14,14 @@ patient_symptom(kate,fever).
 patient_symptom(kate,headache).
 
 :- include('diseases.pl').
+
+% possible symptoms 
+symptoms([abdominal_pain,breathing_problems,chills,cough,fatigue,fever,headache,indigestion]).
+
+list_length([],0).
+list_length([H|T], L+1) :- list_length(T,L).
+
+number_of_possible_symptoms(L) :- list_length(A, K), symptoms(A), L is K.
+
+ask_about_symptoms(0).
+ask_about_symptoms(N) :- N>0, write('Do you have '), symptoms(A), nth1(N,A,X), write(X),  write('?'), nl, M is N-1, ask_about_symptoms(M).
